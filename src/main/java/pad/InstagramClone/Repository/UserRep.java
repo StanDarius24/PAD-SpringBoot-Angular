df0171e957f2273f33c2d613894eca6cb47f30e0
StanDarius24 <standarius24@gmail.com>
2021-03-25 20:39:07 +0200

    fixed

--- src/main/java/pad/InstagramClone/Repository/UserRep.java
+++
@@ -1,9 +0,0 @@
-package pad.InstagramClone.Repository;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import pad.InstagramClone.Users.User;
-@SpringBootApplication
-public interface UserRep extends JpaRepository<User,Integer> {
-    User findByName(String name);
-}
