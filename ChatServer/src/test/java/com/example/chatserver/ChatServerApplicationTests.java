f50008facd1510a0e98d040c54ec2c506cda42d1
StanDarius24 <standarius24@gmail.com>
2021-04-18 19:27:15 +0300

    server chat

--- ChatServer/src/test/java/com/example/chatserver/ChatServerApplicationTests.java
+++ ChatServer/src/test/java/com/example/chatserver/ChatServerApplicationTests.java
@@ -1,13 +1,13 @@
-package pad.SocialMedia;
+package com.example.chatserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SocialMediaApplicationTests {
+class ChatServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
