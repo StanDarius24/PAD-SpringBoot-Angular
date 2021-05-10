8a225db31fe0f76ea5ed1ceb86321cca4c150a2e
StanDarius24 <standarius24@gmail.com>
2021-05-10 16:28:24 +0300

    Done

--- AngularFront/src/app/token-interceptor.ts
+++ AngularFront/src/app/token-interceptor.ts
@@ -18,50 +18,56 @@ export class TokenInterceptor implements HttpInterceptor {
   }
 
   intercept(req:HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (req.url.indexOf('refresh') !== -1 || req.url.indexOf('login') !== -1) {
+      return next.handle(req);
+    }
     const jwtToken = this.authService.getJwtToken();
 
-    if(jwtToken)
-    {
-      this.addToken(req,jwtToken);
-    }
-    return  next.handle(req).pipe(
-      catchError(err => {
-
-        if( err instanceof HttpErrorResponse && err.status === 403)
-        {
-          return this.handleAuthErrors(req,next);
-        } else
-        {
-          return throwError(err);
+    if (jwtToken) {
+      return next.handle(this.addToken(req, jwtToken)).pipe(catchError(error => {
+        if (error instanceof HttpErrorResponse
+          && error.status === 403) {
+          return this.handleAuthErrors(req, next);
+        } else {
+          return throwError(error);
         }
-      })
-    )
-      ;
+      }));
+    }
+    return next.handle(req);
 
   }
 
 
-  private addToken(req: HttpRequest<any>, jwtToken: any) {
+   addToken(req: HttpRequest<any>, jwtToken: any) {
     return req.clone({
       headers: req.headers.set('Authorization', 'Bearer ' + jwtToken)
     });
   }
 
   private handleAuthErrors(req: HttpRequest<any>, next: HttpHandler) {
-    if(!this.isTokenRefreshing)
-    {
+    if (!this.isTokenRefreshing) {
       this.isTokenRefreshing = true;
       this.refreshTokenSubject.next(null);
 
       return this.authService.refreshToken().pipe(
-        switchMap((refreshTokenResponse : LoginResponse) => {
+        switchMap((refreshTokenResponse: LoginResponse) => {
           this.isTokenRefreshing = false;
-          this.refreshTokenSubject.next(
-            refreshTokenResponse.authenticationToken
-          );
-          return next.handle(this.addToken(req,refreshTokenResponse.authenticationToken));
+          this.refreshTokenSubject
+            .next(refreshTokenResponse.authenticationToken);
+          return next.handle(this.addToken(req,
+            refreshTokenResponse.authenticationToken));
         })
       )
+    } else {
+      return this.refreshTokenSubject.pipe(
+        filter(result => result !== null),
+        take(1),
+        switchMap((res) => {
+          return next.handle(this.addToken(req,
+            this.authService.getJwtToken()))
+        })
+      );
     }
   }
 }
